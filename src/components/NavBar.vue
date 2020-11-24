7cba45939670703912b874449835f9ec0594f38d
Killian Galea <kgalea@steamulo.com>
2020-11-24 11:15:02 +0100

    [INIT] Router & Tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div  class="flex justify-center">
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar",
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    Home Page
+</template>
+
+<script>
+    export default {
+        name: "HomeView",
+    }
+</script>
+
+<style>
+
+</style>
