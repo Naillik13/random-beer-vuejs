cc08bb913be791971bc7ec9826c18e541a319757
Killian Galea <kgalea@steamulo.com>
2020-11-26 09:42:19 +0100

    [FEAT] Purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.vue",
+    "./src/**/**/*.vue",
+    "./src/*.vue"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
